8ca87108b84aa00d2327a955bf83e93615cdc8f9
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-08-20 15:18:55 -0500

    java servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,23 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
+
+@WebServlet({"/", "/somewhere"})
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String cohort = req.getParameter("cohort");
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, " + cohort + "!</h1>");
+
+
 
     }
 }
