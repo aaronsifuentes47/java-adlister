a03f37a0927ca021d7cd57ea1fa16cf26297610f
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-08-22 08:59:42 -0500

    added profile and login parts

--- src/main/java/profileServlet.java
+++ src/main/java/profileServlet.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name="profileServlet", urlPatterns = "profile")
+public class profileServlet extends HttpServlet {
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+
     }
 }
---
+++ src/main/webapp/partials/header.jsp
@@ -0,0 +1,12 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Smilez
+  Date: 2019-08-21
+  Time: 19:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<head>
+    <title>Title</title>
+</head>
